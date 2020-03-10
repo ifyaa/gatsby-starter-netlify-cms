c34ab2288efb2d5629e4e664ec12ef01cf373dc7
nasa2135 <nasa2135@gmail.com>
2020-03-10 11:03:47 +0900

    disqus

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,6 +3,18 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date()
+    ;(function() {
+      var s1 = document.createElement('script'),
+        s0 = document.getElementsByTagName('script')[0]
+      s1.async = true
+      s1.src = 'https://embed.tawk.to/5e65e0e9c32b5c19173a4db1/1e2vnlqkc'
+      s1.charset = 'UTF-8'
+      s1.setAttribute('crossorigin', '*')
+      s0.parentNode.insertBefore(s1, s0)
+    })()
+    
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
