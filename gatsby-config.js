e1b8c4c891512af497da3a812fc6cab7e89a3922
nasa2135 <nasa2135@gmail.com>
2020-03-11 09:54:59 +0900

    tawk

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-tawk`,
+      options: {
+        tawkId: '5e65e0e9c32b5c19173a4db1',
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
